0eab89d1462d741a60b89adeff05679d084535c9
AndreyEducativo <andrey.morales.mendoza@est.una.ac.cr>
2026-02-22 13:09:44 -0600

    primer ejercicio de programacion 2

--- Programacion2/Programacion2/Programacion2.cpp
+++ Programacion2/Programacion2/Programacion2.cpp
@@ -1,19 +1,36 @@
-// Programacion2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
+//Primer ejercicio de clases. Hacer una divicion sin usar el operador de division.
 #include <iostream>
-
+using namespace std; 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int dividendo, divisor, resultado = 0, residuo = 0, numero; 
+
+	cout<< "Ingrese el dividendo: ";
+	cin >> dividendo;
+	cout << endl; 
+	cout<< "Ingrese el divisor: ";
+	cin >> divisor;
+	cout << endl; 
+	cout << "Ingrese un numero que multiplicado por " << divisor << " me de o se acerque al " << dividendo << ": ";
+	cin >> numero;
+	cout << endl;
+
+	resultado = divisor * numero;
+
+	if (divisor == 0)
+	{
+		cout << "No se puede dividir por cero." << endl;
+		return 0; 
+	} 
+	if (resultado > dividendo) {
+		cout << "El numero que ingreso es muy grande, ingrese otro valor..." << endl;
+
+	}
+	else if (resultado < dividendo) {
+		residuo = dividendo - resultado; 
+		cout << "El residuo de la division es: " << residuo << endl; 
+		cout << "El resultado de la division es: " << numero << endl;
+	}
+
+	return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
